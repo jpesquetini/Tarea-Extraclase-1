a66e5f7da04b18f677426aa67831d154410f6417
Joae Pablo Esquetini <jpesquetinif@gmail.com>
2021-08-26 06:55:42 -0600

    Inicios de la interfaz

--- Clientes.java
+++ Clientes.java
@@ -1,1 +1,16 @@
+import javax.swing.*;
+public class Clientes{
+    public static void main(String[] args){
+        JFrame window;
 
+        window = new JFrame("Tarea Extraclase 1");
+        window.setSize(450 , 500);
+        window.setVisible(true);
+
+        JButton send;
+
+        send = new JButton("Send");
+        send.setBounds(170, 355 , 100 , 50);
+        window.add(send);
+    }
+}
